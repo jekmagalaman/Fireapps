5eeecd685b0c6b8fd5c2e088bba1f8c76d9e9923
jekmagalaman <202020184@psu.palawan.edu.ph>
2025-04-29 11:28:03 +0800

    Fire Truck, Fire Incidents, Charts

---
+++ projectsite/fire/models.py
@@ -0,0 +1,92 @@
+from django.db import models
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Locations(BaseModel):
+    name = models.CharField(max_length=150)
+    latitude = models.DecimalField(
+        max_digits=22, decimal_places=16, null=True, blank=True)
+    longitude = models.DecimalField(
+        max_digits=22, decimal_places=16, null=True, blank=True)
+    address = models.CharField(max_length=150)
+    city = models.CharField(max_length=150)  # can be in separate table
+    country = models.CharField(max_length=150)  # can be in separate table
+
+    def __str__(self):
+        return self.name
+
+class Incident(BaseModel):
+    SEVERITY_CHOICES = (
+        ('Minor Fire', 'Minor Fire'),
+        ('Moderate Fire', 'Moderate Fire'),
+        ('Major Fire', 'Major Fire'),
+    )
+    location = models.ForeignKey(Locations, on_delete=models.CASCADE)
+    date_time = models.DateField(blank=True, null=True)
+    severity_level = models.CharField(max_length=45, choices=SEVERITY_CHOICES)
+    description = models.CharField(max_length=250)
+
+    def __str__(self):
+        return f"{self.severity_level} at {self.location}"
+
+class FireStation(BaseModel):
+    name = models.CharField(max_length=150)
+    latitude = models.DecimalField(
+        max_digits=22, decimal_places=16, null=True, blank=True)
+    longitude = models.DecimalField(
+        max_digits=22, decimal_places=16, null=True, blank=True)
+    address = models.CharField(max_length=150)
+    city = models.CharField(max_length=150) 
+    country = models.CharField(max_length=150) 
+
+    def __str__(self):
+        return self.name
+
+class Firefighters(BaseModel):
+    RANK_CHOICES = (
+        ('Probationary Firefighter', 'Probationary Firefighter'),
+        ('Firefighter I', 'Firefighter I'),
+        ('Firefighter II', 'Firefighter II'),
+        ('Firefighter III', 'Firefighter III'),
+        ('Driver', 'Driver'),
+        ('Captain', 'Captain'),
+        ('Battalion Chief', 'Battalion Chief'),)
+    
+    XP_CHOICES = (
+        ('Beginner', 'Beginner'),
+        ('Intermediate', 'Intermediate'),
+        ('Advanced', 'Advanced'),
+        ('Expert', 'Expert'),)
+    name = models.CharField(max_length=150)
+    rank = models.CharField(max_length=45, null=True, blank=True, choices=RANK_CHOICES)
+    experience_level = models.CharField(max_length=45, null=True, blank=True, choices=XP_CHOICES)
+    station = models.ForeignKey(FireStation, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class FireTruck(BaseModel):
+    truck_number = models.CharField(max_length=150)
+    model = models.CharField(max_length=150)
+    capacity = models.IntegerField()
+    station = models.ForeignKey(FireStation, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.truck_number
+
+class WeatherConditions(BaseModel):
+    incident = models.ForeignKey(Incident, on_delete=models.CASCADE)
+    temperature = models.DecimalField(max_digits=10, decimal_places=2)
+    humidity = models.DecimalField(max_digits=10, decimal_places=2)
+    wind_speed = models.DecimalField(max_digits=10, decimal_places=2)
+    weather_description = models.CharField(max_length=150)
+
+    def __str__(self):
+        return f"Weather on {self.incident}: {self.weather_description}"
